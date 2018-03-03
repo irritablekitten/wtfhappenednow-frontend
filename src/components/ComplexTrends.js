4a35088ad2be6209c2e67a41dfa576d2d8dafa1e
Samuel Defazio <irritablekitten@gmail.com>
2018-03-02 20:23:53 -0800

    Wordcloud adjustments

--- src/components/ComplexTrends.js
+++ src/components/ComplexTrends.js
@@ -56,14 +56,14 @@ class ComplexTrends extends Component  {
     if (list[0][1] > 100 && list[1][1] > 80 && list[2][1] > 70 && list[3][1] > 60 && list[4][1]) {
       return 0.5;
     }
-    else if (list[0][1] > 50 && list[19][1] > 30 && list[29][1] > 20)  {
+    else if (list[0][1] > 70 && list[19][1] > 40 && list[29][1] > 30)  {
       return 1;
     }
     else if (list[0][1] > 40 && list[19][1] > 20 && list[29][1] > 10) {
-      return 2;
+      return 1.2;
     }
     else {
-      return 3;
+      return 1.5;
     }
   }
 
