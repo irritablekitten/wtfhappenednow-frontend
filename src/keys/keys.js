84da97958ed0f21eaf90402b8db7d871d38a9ce8
Samuel Defazio <irritablekitten@gmail.com>
2018-03-03 23:56:38 -0800

    Repo setup

--- src/keys/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyBeRAokgD-thqr-Wm_w9Uc2Xt6asixWrpA",
-    authDomain: "wtfhappenednow-server.firebaseapp.com",
-    databaseURL: "https://wtfhappenednow-server.firebaseio.com",
-    projectId: "wtfhappenednow-server",
-    storageBucket: "wtfhappenednow-server.appspot.com",
-    messagingSenderId: "666529709420"
-  };
